0de21b9f809c7948631a149bb46269dd6d749621
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-12-27 23:42:24 +0100

    Fix githash.sh

--- githash.sh
+++ githash.sh
@@ -2,4 +2,7 @@
 
 GITHASH=$(git describe --always --dirty 2> /dev/null || echo "NOGIT")
 
+echo -e "Writing to $1:\n"
 echo -e "#ifndef GIT_HASH\n#define GIT_HASH \""${GITHASH}\""\n#endif"
+
+echo -e "#ifndef GIT_HASH\n#define GIT_HASH \""${GITHASH}\""\n#endif" > "$1"
